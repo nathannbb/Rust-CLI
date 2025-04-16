cd4f83a2e8cff5374e7f9c1dfa14dce9adc1562d
Nathan <nathanbasant.nb@gmail.com>
2025-04-16 15:56:25 -0400

    Error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,31 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In the file {}",config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong");
+    if let Err(e) = run(config){
+        print!("Application error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("Text: \n{}", contents);
 
+    Ok(())
 }
 
 struct Config {
@@ -21,11 +34,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config{
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() < 3{
+            return Err("Arguments missing, try with \"query\" \"filename\"");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config{ query, filename }
+        Ok(Config{ query, filename })
     }
 }
 
