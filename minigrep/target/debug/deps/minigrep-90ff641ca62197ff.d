fc2c0d583d64842f4d444bce4509f7fb60dc2ae7
Nathan <nathanbasant.nb@gmail.com>
2025-04-16 15:26:54 -0400

    Implemented user input and file reading

---
+++ minigrep/target/debug/deps/minigrep-90ff641ca62197ff.d
@@ -0,0 +1,5 @@
+/Users/nathan/Documents/GitHub/Rust-CLI/minigrep/target/debug/deps/minigrep-90ff641ca62197ff: src/main.rs
+
+/Users/nathan/Documents/GitHub/Rust-CLI/minigrep/target/debug/deps/minigrep-90ff641ca62197ff.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/Users/nathan/Documents/GitHub/Rust-CLI/minigrep/target/debug/minigrep: /Users/nathan/Documents/GitHub/Rust-CLI/minigrep/src/main.rs
